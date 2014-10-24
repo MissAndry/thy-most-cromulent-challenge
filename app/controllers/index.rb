05fc06b886b6410fe14383281e60cf07d063a56f
Megan Gaebler <megan.gaebler@gmail.com>
2014-10-24 16:59:05 -0500

    Ajax working, style still needed

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,14 +2,14 @@ get '/' do
   erb :index
 end
 
-get '/generate-insult/:insult_id' do
-  @shakes = Parser.new('app/helpers/shakespeare-insults.txt').generate_insult
+get '/generate-insult' do
+  @shakes = Parser.new('app/helpers/shakespearean-insults.txt').generate_insult
   @insult = Insult.find_or_create_by(text: @shakes)
   erb :_insult, layout: false
 end
 
 post '/send-insult/:insult_id' do
   @insult = Insult.find(params[:insult_id])
-  SendSMS.new(@insult).send_sms
+  SendSMS.new(@insult.text).send_sms
   erb :_thanks, layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("a").on("click", function(event){
+    event.preventDefault();
+
+    $.ajax({
+      url: $(this).attr("href"),
+      type: "get",
+      success: function(response){
+        console.log("ding!");
+        $("#send-form").html(response);
+      }
+    });
+  });
+
+  $("#send-form").on("submit", "#send", function(event){
+    event.preventDefault();
+
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+    console.log(url);
+    console.log(data);
+
+
+    $.ajax({
+      url: url,
+      type: "post",
+      data: data,
+      success: function(response){
+        console.log("ding!");
+        console.log($(this));
+        console.log($(this).parent());
+        $("#send-form").parent().html(response);
+      }
+    });
+  });
+
 });
