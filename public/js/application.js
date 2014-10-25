76a7ea349f65c0616afe3d2b4a99768705dae9d7
Megan Gaebler <megan.gaebler@gmail.com>
2014-10-25 14:43:12 -0500

    Finished stylin

--- public/js/application.js
+++ public/js/application.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
       success: function(response){
         console.log("ding!");
         $("#send-form").html(response);
+        $("#instructions").hide();
       }
     });
   });
