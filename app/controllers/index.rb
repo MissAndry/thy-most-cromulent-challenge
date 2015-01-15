ed58a81cb983c03829601cfa9ca949415c33dacb
Megan Gaebler <megan.gaebler@gmail.com>
2015-01-15 00:07:01 -0600

    updated routes to follow RESTful conventions

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,13 +2,13 @@ get '/' do
   erb :index
 end
 
-get '/generate-insult' do
+get '/insults/create' do
   @shakes = Parser.new('app/helpers/shakespearean-insults.txt').generate_insult
   @insult = Insult.find_or_create_by(text: @shakes)
   erb :_insult, layout: false
 end
 
-post '/send-insult/:insult_id' do
+post '/insults/:insult_id/send' do
   @insult = Insult.find(params[:insult_id])
   SendSMS.new(@insult.text).send_sms
   erb :_thanks, layout: false
